2eacd7db8946873781a40c46bc67f0219d9d4dcb
Ivan <j2test.git@gmail.com>
2018-10-09 14:55:40 +0300

    JL34GC exmple with 10000000 obj

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
-public class Main
-{
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hellow world");
+  /*      Date dt = method2();
+        dt = null;
+        System.gc();*/
+        method();
+
+
     }
 
+    private static void method() {
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println("total memory before " + runtime.totalMemory());
+        System.out.println("free memory before " + runtime.freeMemory());
+        List<Date> dateList=new ArrayList<>();
+        for (int i = 0; i < 10000000; i++) {
+            Date date = new Date();
+            dateList.add(date);
+         //   date = null;
+
+        }
+        System.out.println("total memory after " + runtime.totalMemory());
+        System.out.println("free memory after " + runtime.freeMemory());
+        System.gc();
+        System.out.println("total memory after GC " + runtime.totalMemory());
+        System.out.println("free memory after GC " + runtime.freeMemory());
+    }
+
+    private static Date method2() {
+
+        Date date = new Date();
+        Date date2 = date;
+        System.out.println(date);
+        return date;
+    }
 }
